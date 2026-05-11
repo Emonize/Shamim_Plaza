8d018fe590cc7023eb538a38f2fa021726bec4ad
emonize <yeasinarafat72@gmail.com>
2026-05-11 17:23:03 -0400

    chore: save sanity deploy config

--- shamim-studio/sanity.cli.js
+++ shamim-studio/sanity.cli.js
@@ -5,7 +5,9 @@ export default defineCliConfig({
     projectId: '2za38opa',
     dataset: 'production'
   },
+  studioHost: 'shamimplaza',
   deployment: {
+    appId: 'gemn800y7nfmep2vqxblamlu',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
