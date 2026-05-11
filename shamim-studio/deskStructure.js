011b0350338147e6ccef353cb2a477addd88a807
emonize <yeasinarafat72@gmail.com>
2026-05-11 08:40:45 -0400

    feat: configure custom desk structure to group site settings

---
+++ shamim-studio/deskStructure.js
@@ -0,0 +1,26 @@
+export const myStructure = (S) =>
+  S.list()
+    .title('Shamim Plaza Content')
+    .items([
+      S.listItem()
+        .title('Site Settings')
+        .child(
+          S.list()
+            .title('Settings')
+            .items([
+              S.listItem()
+                .title('Homepage Settings')
+                .child(S.document().schemaType('homepageSettings').documentId('f31314de-af0c-4673-a79c-f6b4764043b9')),
+              S.listItem()
+                .title('About Us Settings')
+                .child(S.document().schemaType('aboutSettings').documentId('c1f96aa0-3630-4f06-8672-c5d9d0ebffc1')),
+              S.listItem()
+                .title('Contact Settings')
+                .child(S.document().schemaType('contactSettings').documentId('25f11050-d7f8-4af9-aea6-e67fe9b7bb98')),
+            ])
+        ),
+      S.divider(),
+      ...S.documentTypeListItems().filter(listItem => 
+        !['homepageSettings', 'aboutSettings', 'contactSettings'].includes(listItem.getId())
+      )
+    ])
